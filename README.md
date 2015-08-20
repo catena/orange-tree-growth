47df6363d7fc6fe12f9ca830b72244f3d2ba9d23
catena <vishnusuresh1729@gmail.com>
2015-08-20 12:42:45 +0530

    Update README, add comments, cleanup

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# orange-tree-growth
-Orange Tree Growth - Developing Data Products Course Project
+
+# Orange Tree Growth Estimator
+
+
+## Overview and Usage
+
+This shiny application can be used to estimate the growth of an orange tree. It uses the Orange dataset and fits a non linear least squares model to estimate the tree growth. Please note this is an experimental tool and may not be accurate.  
+
+Use the slider to select the age of the orange tree. The estimated tree height will be displayed instantly. The tree height is reported as circumference at breast height, which is a standard measurement in forestry.  
+
+To see it in live, see [this][1] rpubs link.  
+
+
+Note: **Requires** shiny 0.12.2 and plotly 1.0.3.
+
+
+[1]: https://catena.shinyapps.io/orange-tree 
--- server.R
+++ server.R
@@ -4,9 +4,11 @@ library(plotly)
 
 shinyServer(function(input, output) {
     
-    growth <- reactive({ predict(fm1, list(age = input$ageInput)) })
+    # predicted value of circumference
+    growth <- reactive({ predict(fit, list(age = input$ageInput)) })
     
     output$trendPlot <- renderPlotly({
+        # update plot markers
         age.in <- input$ageInput
         p %>% add_trace(y = c(0, growth(), 250), x = rep(age.in, 3), showlegend = F,
                         line = list(color = "black", width = 2, dash = "solid"))
@@ -17,22 +19,17 @@ shinyServer(function(input, output) {
     })
 })
 
-fm1 <- nls(circumference ~ SSlogis(age, Asym, xmid, scale), data = Orange)
+# fit using nls model
+fit <- nls(circumference ~ SSlogis(age, Asym, xmid, scale), data = Orange)
 
+# create plotly plot
 age.pred <- seq(20, max(1600), len = 100)
 p <- plot_ly(x = age, y = circumference, data = Orange, group = Tree,
              line = list(width = 1, opacity = 0.8)) %>%
-    layout(title = "Orange tree data and fitted model",
+    layout(title = "Orange tree data and fitted model. (Trunk circumference of 5 trees measured at 7 different ages)",
            xaxis = list(title = "Age (days)"),
            yaxis = list(title = "Circumference (mm)")) %>% 
-    add_trace(y = predict(fm1, list(age = unique(age.pred))), x = age.pred,
+    add_trace(y = predict(fit, list(age = unique(age.pred))), x = age.pred,
               group = "fitted",
               line = list(shape = "spline", width = 3, dash = "solid"))
 
-# g <- qplot(age, circumference, data = Orange, geom = c("point", "line"), 
-#            color = Tree, xlab = "Age (days)", ylab = "Circumference (mm)",
-#            main = "Growth of Orange Trees")
-# age <- seq(0, 1600, length.out = 101)
-# g <- g + geom_line(aes(x = age, y = predict(fm1, list(age = age))), 
-#                    size = 2, color = "#777777")
-# g
